6b8e36abab35ede747859f52a1ee5d0476e4a774
VladimirFilippov10 <asdqwer11zx@gmail.com>
2024-12-09 14:02:38 +0500

    мелкие исправления

--- timesheetForClients/MainWindow.xaml.cs
+++ timesheetForClients/MainWindow.xaml.cs
@@ -52,8 +52,8 @@ namespace timesheetForClients
         {
             if (!string.IsNullOrEmpty(selectedFolder) && !string.IsNullOrEmpty(dateStart))
             {
-                var files = Directory.GetFiles(selectedFolder, "*employee*.xlsm").ToList();
-                HashSet<string> uniqueProjects = new HashSet<string>();
+                var files = Directory.GetFiles(selectedFolder, "*employee*.xlsm").ToList(); // получение всех файлов работников
+                HashSet<string> uniqueProjects = new HashSet<string>(); // список проектов
 
                 // Сбор уникальных проектов
                 foreach (var file in files)
@@ -63,6 +63,7 @@ namespace timesheetForClients
                         var worksheet = workbook.Worksheet(1); // Получаем первый лист
                         int rowCount = worksheet.LastRowUsed().RowNumber(); // Получаем количество строк
                         int startRow = -1;
+                        // получение стилей
                         colorHeaderB = XLColor.FromColor(worksheet.Cell("C10").Style.Fill.BackgroundColor.Color);
                         colorDayB = XLColor.FromColor(worksheet.Cell("C14").Style.Fill.BackgroundColor.Color);
                         colorResultHeaderB = XLColor.FromColor(worksheet.Cell("O10").Style.Fill.BackgroundColor.Color);
@@ -70,27 +71,27 @@ namespace timesheetForClients
                         //MessageBox.Show(colorHeaderB);
                         for (int row = 1; row <= rowCount; row++)
                         {
-                            var cellValue = worksheet.Cell(row, 15).GetString(); // Получаем значение ячейки
+                            var cellValue = worksheet.Cell(row, 15).GetString(); 
                             if (!string.IsNullOrEmpty(cellValue) && cellValue.Trim() == dateStart.Trim())
                             {
-                                startRow = row; // Запоминаем строку, где найдено совпадение
-                                break; // Прерываем цикл, так как первое совпадение найдено
+                                startRow = row; 
+                                break; 
                             }
                         }
 
-                        if (startRow != -1) // Если совпадение найдено, собираем названия проектов
+                        if (startRow != -1) 
                         {
-                            for (int row = startRow + 1; row <= rowCount; row++) // Начинаем с строки под найденной
+                            for (int row = startRow + 1; row <= rowCount; row++) 
                             {
-                                var projectNameCell = worksheet.Cell(row, 3).GetString().Trim(); // Получаем значение ячейки с названием проекта
+                                var projectNameCell = worksheet.Cell(row, 3).GetString().Trim(); 
                                 if (!string.IsNullOrEmpty(projectNameCell) && !projectNameCell.Equals("ПРОЕКТ", StringComparison.OrdinalIgnoreCase))
-                                    uniqueProjects.Add(projectNameCell); // Добавляем проект в коллекцию уникальных проектов
+                                    uniqueProjects.Add(projectNameCell); // добавление проекта в список если он уникален
                             }
                         }
                     }
                 }
 
-                // Создание таймшитов 
+                // Создание отчётов по проектам 
                 foreach (var projectName in uniqueProjects)
                 {
                     string newFileName = System.IO.Path.Combine(selectedFolder, $"timesheet_customer_{projectName}.xlsx");
@@ -98,7 +99,7 @@ namespace timesheetForClients
                         createTimesheet(newFileName, projectName, dateStart);
                 }
 
-                // Теперь проходим по файлам с неделями и копируем строки с задачами в файлы проектов
+                // копирование задач
                 foreach (var file in files)
                 {
                     using (var workbook = new XLWorkbook(file))
@@ -107,13 +108,13 @@ namespace timesheetForClients
                         int rowCount = worksheet.LastRowUsed().RowNumber();
                         for (int row = 1; row <= rowCount; row++)
                         {
-                            var projectNameCell = worksheet.Cell(row, 3).GetString().Trim(); // Получаем значение ячейки с названием проекта
+                            var projectNameCell = worksheet.Cell(row, 3).GetString().Trim(); 
                             if (uniqueProjects.Contains(projectNameCell))
-                                copyTaskRow(projectNameCell, worksheet, row);
+                                copyTaskRow(projectNameCell, worksheet, row); // копирование найденной задачи
                         }
                     }
                 }
-                foreach (var projectName in uniqueProjects)
+                foreach (var projectName in uniqueProjects) // формирование итогов и стилей
                 {
                     formingTheResults(projectName);
                 }
@@ -187,54 +188,52 @@ namespace timesheetForClients
             {
                 using (var workbook = new XLWorkbook(newFileName))
                 {
-                    var worksheetProject = workbook.Worksheets.First(); // Получаем первый лист
-                    int lastRow = worksheetProject.LastRowUsed().RowNumber(); // Получаем количество строк
-                    var taskName = worksheet.Cell(row, 4).GetString().Trim(); // Получаем значение ячейки с названием задачи из текущей строки
+                    var worksheetProject = workbook.Worksheets.First(); 
+                    int lastRow = worksheetProject.LastRowUsed().RowNumber();
+                    var taskName = worksheet.Cell(row, 4).GetString().Trim();
                     bool taskExists = false;
 
                     for (int i = 1; i <= lastRow; i++)
                     {
-                        var existingTaskName = worksheetProject.Cell(i, 4).GetString().Trim(); // Получаем значение ячейки с названием задачи
+                        var existingTaskName = worksheetProject.Cell(i, 4).GetString().Trim();
                         if (existingTaskName.Equals(taskName, StringComparison.OrdinalIgnoreCase))
                         {
                             taskExists = true;
-                            // Суммируем часы
+                            // сумирование часов
                             for (int j = 8; j <= 14; j++)
                             {
                                 double existingHours = 0;
-                                // Используем TryGetValue для безопасного получения существующих часов
                                 if (worksheetProject.Cell(i, j).TryGetValue<double>(out existingHours))
                                 {
                                     double newHours = 0;
 
-                                    // Используем TryGetValue для безопасного получения нового значения из следующей строки
-                                    if (worksheet.Cell(row + 1, j).TryGetValue<double>(out newHours)) // Изменено на row + 1
-                                        worksheetProject.Cell(i, j).Value = existingHours + newHours; // Суммируем часы
+                                    if (worksheet.Cell(row + 1, j).TryGetValue<double>(out newHours)) 
+                                        worksheetProject.Cell(i, j).Value = existingHours + newHours; 
                                 }
                             }
                             break;
                         }
                     }
 
-                    if (!taskExists)
+                    if (!taskExists) // если такой задачи нет
                     {
-                        // Копируем строку с задачей в файл проекта в 4-й столбец
-                        worksheetProject.Cell(lastRow + 1, 4).Value = taskName; // Название задачи в 4-й столбец
+                        // копирование строки с задачей
+                        worksheetProject.Cell(lastRow + 1, 4).Value = taskName; 
 
                         worksheetProject.Range(worksheetProject.Cell(lastRow + 1, 4), worksheetProject.Cell(lastRow + 1, 7)).Merge().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
 
-                        // Копируем часы для каждого дня из следующей строки в столбцы H (8) по N (14)
+                        // копирование часов
                         for (int j = 8; j <= 14; j++)
                         {
                             worksheetProject.Column(j).Width = 15;
                             double hours = 0;
-                            // Проверяем, является ли значение числом
-                            if (worksheet.Cell(row + 1, j).TryGetValue<double>(out hours)) // Изменено на row + 1
-                                worksheetProject.Cell(lastRow + 1, j).Value = hours; // Копируем часы для каждого дня из следующей строки
+                            if (worksheet.Cell(row + 1, j).TryGetValue<double>(out hours)) 
+                                worksheetProject.Cell(lastRow + 1, j).Value = hours; 
                             else
-                                worksheetProject.Cell(lastRow + 1, j).Value = 0; // Если значение не число, устанавливаем 0
+                                worksheetProject.Cell(lastRow + 1, j).Value = 0; 
                             worksheetProject.Cell(lastRow + 1, j).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
                         }
+                        // итоги право
                         worksheetProject.Cell(lastRow + 1, 15).FormulaA1 = $"SUM(H{lastRow + 1}:N{lastRow + 1})";
                         worksheetProject.Cell(lastRow + 1, 15).Style.Fill.BackgroundColor = colorResultB;
                         worksheetProject.Cell(lastRow + 1, 15).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
@@ -258,10 +257,9 @@ namespace timesheetForClients
                     worksheetProject.Cell(lastRow + 1, 4).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
                     worksheetProject.Cell(lastRow + 1, 4).Style.Fill.BackgroundColor = colorDayB;
                     worksheetProject.Cell(lastRow + 1, 4).Style.Font.FontColor = XLColor.White;
-                    // Суммируем часы по столбцам с H (8) по N (14)
                     for (int j = 8; j <= 15; j++)
                     {
-                        // Формула для суммирования значений от 10 строки до последней
+                        // итоги низ
                         string columnLetter = worksheetProject.Column(j).ColumnLetter();
                         worksheetProject.Cell(lastRow + 1, j).FormulaA1 = $"SUM({columnLetter}10:{columnLetter}{lastRow})";
                         worksheetProject.Cell(lastRow + 1, j).Style.Fill.BackgroundColor = colorDayB;
